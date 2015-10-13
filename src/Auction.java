b22b12c5c5fc66693989b44ad816ee71ef617ef7
ZaraTunyan <zara.tunyan@gmial.com>
2015-10-13 15:50:47 +0400

    Update Auction.java

--- src/Auction.java
+++ src/Auction.java
@@ -1,51 +1,58 @@
 import com.sun.org.apache.bcel.internal.generic.NEW;
 import com.sun.org.apache.xpath.internal.operations.Bool;
 
+
 import java.util.ArrayList;
-import java.util.GregorianCalendar;
-import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.util.UUID;
 
 
 public class Auction implements Runnable{
 
-    private String AuctionID;
-    private GregorianCalendar StartDate = new GregorianCalendar();
-    private GregorianCalendar EndDate;
+    private UUID AuctionID;
+    private Date StartDate = new Date();
+    private Date EndDate;
     private String State = "active";
     private int SellerID;
-    private Double InitialPrice;
-    private Double MinBidPrice;
-    private Double BuyItNowPrice;
+    private double InitialPrice;
+    private double MinBidPrice;
+    private double BuyItNowPrice;
     private volatile Double CurrentPrice;
-    private Double bidprice;
+    private double bidprice;
+    private String auctionID;
     private Boolean run = false;
     private ArrayList<Bid> bids = new ArrayList<Bid>();
 
-    public Auction(GregorianCalendar newEndDate, int newSellerID, Double newInitialPrice, Double newMinBidPrice,
-                   Double newBuyItNowPrice) {
+    public Auction(Date newEndDate, int newSellerID, double newInitialPrice, double newMinBidPrice,
+                   double newBuyItNowPrice) {
         EndDate = newEndDate;
         SellerID = newSellerID;
         InitialPrice = newInitialPrice;
         MinBidPrice = newMinBidPrice;
         BuyItNowPrice = newBuyItNowPrice;
         CurrentPrice = newInitialPrice;
+        AuctionID = UUID.randomUUID();
+
     }
 
-    private Boolean ChekBidder(int BidderID){
+    private Boolean CheckBidder(int BidderID){
         if (this.SellerID != BidderID)
             return true;
         else return false;
     }
 
     private Boolean CheckDate() {
-        GregorianCalendar currentdate = new GregorianCalendar();
-        if (currentdate.before(EndDate))
-            return true;
+        Date currentDate = new Date();
+        int result = currentDate.compareTo(EndDate);
+        if(result > 0){
+            System.out.println("Auction date is out");
+            return false;}
         else
-            return false;
+            System.out.println("Current Date:" + currentDate +"\n"+ "End Date:" + EndDate );
+        return true;
     }
 
-    private void setBidprice(Double NewBidprice) {
+    private void setBidprice(double NewBidprice) {
         bidprice = NewBidprice;
     }
 
@@ -62,21 +69,21 @@ public class Auction implements Runnable{
 
     }
 
-    public Boolean setBid(Double bidPreice, int bidderID) {
-        if (ChekBidder(bidderID) & CheckDate()) {
-            Bid newBid = new Bid(this.AuctionID, bidderID, bidPreice);
+    public Boolean setBid(double bidPrice, int bidderID) {
+        if (CheckBidder(bidderID) & CheckDate()) {
+            Bid newBid = new Bid(this.AuctionID, bidderID, bidPrice);
             bids.add(newBid);
-            setBidprice(bidPreice);
+            setBidprice(bidPrice);
             run = true;
             return true;
         }
         else return false;
     }
 
-    public String getAuctionID(){
+    public UUID getAuctionID(){
         return AuctionID;
     }
-    public Double getCurrentPrice(){return CurrentPrice;}
+    public double getCurrentPrice(){return CurrentPrice;}
 
 
 }
